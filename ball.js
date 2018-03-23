314448691c2d5cb32657a6d80b4f817c502c2c36
Dannydjay <danny_djay@yahoo.com>
2018-03-23 18:17:09 +0200

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,18 +1,19 @@
 
 document.getElementById("id_business_level").innerHTML = "Business version = 2018.03.23.1";
 
+var stare = {y:0, directie: 1};
 
-setInterval(deseneaza, 30);
+setInterval(deseneaza, 30, stare);
 
-function deseneaza()
+function deseneaza(x)
 {
 var t = document.getElementById("id_transform");
-t.setAttribute("translation", "0 " + y + " 0");
-y+=directie;
-if (y> 10)
-	directie = -1;
+t.setAttribute("translation", "0 " + x.y + " 0");
+x.y+=directie;
+if (x.y> 10)
+	x.directie = -1;
 else
-	if (y < 0)
-		directie = 1;
+	if (x.y < 0)
+		x.directie = 1;
 
 }
