90545785180f0166711e35188b22c69707ff152d
Dannydjay <danny_djay@yahoo.com>
2018-03-23 18:06:21 +0200

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,13 +1,13 @@
 
 setInterval(deseneaza, 30);
-var y=0
-var directie=1
+var y=0;
+var directie=1;
 function deseneaza()
 {
 var t = document.getElementById("id_transform");
 t.setAttribute("translation", "0 " + y + " 0");
 y+=directie;
-if (y> 10)
+if (y > 10)
 	directie = -1;
 else
 	if (y < 0)
